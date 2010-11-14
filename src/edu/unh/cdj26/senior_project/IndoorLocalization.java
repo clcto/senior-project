20d322adac5e48a0edf9739ebc4ff91f5cdda637
Carick Wienke <cdwienke@gmail.com>
2010-11-14 13:03:39 -0500

    start: from android create project

---
+++ src/edu/unh/cdj26/senior_project/IndoorLocalization.java
@@ -0,0 +1,15 @@
+package edu.unh.cdj26.senior_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IndoorLocalization extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
