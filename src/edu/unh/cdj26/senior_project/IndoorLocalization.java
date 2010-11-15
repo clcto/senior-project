331c893df7158ca2f79f564dff032d10b4c3af2e
carick <cdwienke@gmail.com>
2010-11-15 17:54:47 -0500

    .

--- src/edu/unh/cdj26/senior_project/IndoorLocalization.java
+++ src/edu/unh/cdj26/senior_project/IndoorLocalization.java
@@ -2,6 +2,10 @@ package edu.unh.cdj26.senior_project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.net.wifi.*;
+import android.content.Context;
+import java.util.List;
 
 public class IndoorLocalization extends Activity
 {
@@ -10,6 +14,30 @@ public class IndoorLocalization extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView( this );
+        tv.setText( "start" );
+
+        Context c = getApplicationContext();
+        if( c != null )
+        {
+           tv.setText( "c != null" );
+           WifiManager wm = 
+               (WifiManager) c.getSystemService( Context.WIFI_SERVICE );
+           if( wm != null )
+           {
+              tv.setText( "wm != null" );
+              wm.startScan();
+              try
+              {
+                 Thread.sleep( 10000 );
+              }
+              catch( Exception e ){}
+              List<ScanResult> networks = wm.getScanResults();
+
+              tv.setText( "Number of networks: " + networks.size() );
+           }
+        }
+
+        setContentView( tv );
     }
 }
